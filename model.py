81eb0c506d74488ab5965601b8f8cccf8c2473db
SangwookWoo <wjddm3@naver.com>
2023-02-15 17:55:25 +0900

    time

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
